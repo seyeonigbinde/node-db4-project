8ceac2505163b5b479151a78b6ce1d8522fee4eb
Your Name <you@example.com>
2021-06-18 02:23:19 +0100

    Code Clean up

--- api/server.test.js
+++ api/server.test.js
@@ -25,14 +25,3 @@ describe('[GET] /:recipe_id', () => {
     expect(res.body).toMatchObject({ recipe_id: 1, recipe_name: "Ofada Rice"})
   })
 })
-// describe('[POST] /hobbit', () => {
-//   it('returns a status 201 CREATED', async () => {
-//     const res = await request(server).post('/hobbits').send({ name: 'bilbo' })
-//     expect(res.status).toBe(201)
-//   })
-//   it('returns newly created hobbit', async () => {
-//     const res = await request(server).post('/hobbits').send({ name: 'bilbo' })
-//     // console.log(res)
-//     expect(res.body).toMatchObject({ id: 5, name: 'bilbo' })
-//   })
-// })
